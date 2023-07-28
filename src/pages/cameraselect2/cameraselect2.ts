ce3537be4df4cd510ff6e6876add3926e6768807
kotran88 <kotran@naver.com>
2023-07-28 18:23:46 +0900

    불필요 플러그인삭제
    불필요 플러그인삭제

--- src/pages/cameraselect2/cameraselect2.ts
+++ src/pages/cameraselect2/cameraselect2.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController,LoadingController, NavParams,ViewController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 
 /**
@@ -18,101 +17,102 @@ import { ImagePicker } from '@ionic-native/image-picker/ngx';
 export class Cameraselect2Page {
   // imagePicker:any;
   flag:any;
-  constructor(private imagePicker: ImagePicker,private camera: Camera,public loading:LoadingController,public viewCtrl: ViewController,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private camera: Camera,public loading:LoadingController,public viewCtrl: ViewController,public navCtrl: NavController, public navParams: NavParams) {
 
   }
-  getPhoto(){
-    console.log("get photo come ");
 
-    const options = {
-      maximumImagesCount: 100,
-      quality:100,
-      outputType:1
-    };
-    setTimeout(()=>{
-      // this.ip.getPictures(options).then((results) => {
+  // getPhoto(){
+  //   console.log("get photo come ");
 
-      //   console.log(results);
-      //   for (var i = 0; i < results.length; i++) {
-      //       console.log('Image URI: ' + results[i]);
-      //   }
-      //   this.viewCtrl.dismiss({"flag":"multi","data":results});
-      // }, (err) => { console.log(err)});
-    },1000)
-  }
-  getSinglePhoto(){
+  //   const options = {
+  //     maximumImagesCount: 100,
+  //     quality:100,
+  //     outputType:1
+  //   };
+  //   setTimeout(()=>{
+  //     // this.ip.getPictures(options).then((results) => {
+
+  //     //   console.log(results);
+  //     //   for (var i = 0; i < results.length; i++) {
+  //     //       console.log('Image URI: ' + results[i]);
+  //     //   }
+  //     //   this.viewCtrl.dismiss({"flag":"multi","data":results});
+  //     // }, (err) => { console.log(err)});
+  //   },1000)
+  // }
+  // getSinglePhoto(){
 
 
-    const options2: CameraOptions = {
-      quality: 50,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true
-    }
-    const options3 = {
-      // max images to be selected, defaults to 15. If this is set to 1, upon
-      // selection of a single image, the plugin will return it.
-      maximumImagesCount: 100,
-      quality: 50,
-      // max width and height to allow the images to be.  Will keep aspect
-      // ratio no matter what.  So if both are 800, the returned image
-      // will be at most 800 pixels wide and 800 pixels tall.  If the width is
-      // 800 and height 0 the image will be 800 pixels wide if the source
-      // is at least that wide.
-      width: 600,
-      height: 600,
-      outputType:1
+  //   const options2: CameraOptions = {
+  //     quality: 50,
+  //     sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+  //     destinationType: this.camera.DestinationType.DATA_URL,
+  //     encodingType: this.camera.EncodingType.JPEG,
+  //     mediaType: this.camera.MediaType.PICTURE,
+  //     correctOrientation: true
+  //   }
+  //   const options3 = {
+  //     // max images to be selected, defaults to 15. If this is set to 1, upon
+  //     // selection of a single image, the plugin will return it.
+  //     maximumImagesCount: 100,
+  //     quality: 50,
+  //     // max width and height to allow the images to be.  Will keep aspect
+  //     // ratio no matter what.  So if both are 800, the returned image
+  //     // will be at most 800 pixels wide and 800 pixels tall.  If the width is
+  //     // 800 and height 0 the image will be 800 pixels wide if the source
+  //     // is at least that wide.
+  //     width: 600,
+  //     height: 600,
+  //     outputType:1
 
-      // quality of resized image, defaults to 100
-    };
-    console.log("get photo come!")
-    this.imagePicker.getPictures(options3).then((results) => {
-      console.log(results);
-      if(results[0].length<10) {
-        this.viewCtrl.dismiss({"flag":"false","data":undefined})
-      }
-      for (var i = 0; i < results.length; i++) {
-          console.log('Image URI: ' + results[i]);
-      }
-      this.viewCtrl.dismiss({"flag":"multi","data":results});
-    }, (err) => {
-      console.log("err si")
-      console.log(err)
-      this.viewCtrl.dismiss({"flag":"false","data":undefined})
-    });
-  }
+  //     // quality of resized image, defaults to 100
+  //   };
+  //   console.log("get photo come!")
+  //   this.imagePicker.getPictures(options3).then((results) => {
+  //     console.log(results);
+  //     if(results[0].length<10) {
+  //       this.viewCtrl.dismiss({"flag":"false","data":undefined})
+  //     }
+  //     for (var i = 0; i < results.length; i++) {
+  //         console.log('Image URI: ' + results[i]);
+  //     }
+  //     this.viewCtrl.dismiss({"flag":"multi","data":results});
+  //   }, (err) => {
+  //     console.log("err si")
+  //     console.log(err)
+  //     this.viewCtrl.dismiss({"flag":"false","data":undefined})
+  //   });
+  // }
 
-  takePhoto(){
-    console.log("Take photo!!!!");
-    try{
-      const options : CameraOptions={
-        quality:50,
-        targetHeight:600,
-        targetWidth:600,
-        sourceType: this.camera.PictureSourceType.CAMERA,
-        destinationType:this.camera.DestinationType.DATA_URL,
-        encodingType:this.camera.EncodingType.JPEG,
-        mediaType:this.camera.MediaType.PICTURE,
-        saveToPhotoAlbum:true
-      }
-      const result= this.camera.getPicture(options).then(imagedata=>{
-        var temp=[];
-        temp[0]=imagedata;
-        this.viewCtrl.dismiss({'data':temp,"flag":"multi"});
-      })
-    }catch(e){
-      console.log("error "+e);
-      this.viewCtrl.dismiss({"flag":"false","data":undefined})
-    }
-  }
+  // takePhoto(){
+  //   console.log("Take photo!!!!");
+  //   try{
+  //     const options : CameraOptions={
+  //       quality:50,
+  //       targetHeight:600,
+  //       targetWidth:600,
+  //       sourceType: this.camera.PictureSourceType.CAMERA,
+  //       destinationType:this.camera.DestinationType.DATA_URL,
+  //       encodingType:this.camera.EncodingType.JPEG,
+  //       mediaType:this.camera.MediaType.PICTURE,
+  //       saveToPhotoAlbum:true
+  //     }
+  //     const result= this.camera.getPicture(options).then(imagedata=>{
+  //       var temp=[];
+  //       temp[0]=imagedata;
+  //       this.viewCtrl.dismiss({'data':temp,"flag":"multi"});
+  //     })
+  //   }catch(e){
+  //     console.log("error "+e);
+  //     this.viewCtrl.dismiss({"flag":"false","data":undefined})
+  //   }
+  // }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraselectPage22222');
-  }
-  confirm(){
-    this.viewCtrl.dismiss();
-  }
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad CameraselectPage22222');
+  // }
+  // confirm(){
+  //   this.viewCtrl.dismiss();
+  // }
 
 }
